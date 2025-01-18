5d0f2b6371f1c8fabb89c99d22858e7cfca15cff
rteja6935 <rteja6935@gmail.com>
2025-01-17 18:45:34 -0800

    Add initial files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mclient = require('mongodb').MongoClient;
+const cors = require('cors');
+require('dotenv').config();
+
+
+const DBurl =  process.env.DBurl;
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+mclient.connect(DBurl)
+.then((client)=>{
+    const dbobj = client.db('fronix');
+})
+.catch((error)=>{console.log("error in connecting DB",error)});
+
+
+
+
+
+
+
+
+app.listen(PORT,()=>{console.log(`port is listening to ${PORT}`);})
